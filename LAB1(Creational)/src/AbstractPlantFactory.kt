afefc2623b819ab068bf768d7c5a42f64c1f89b5
Vdovicenco Alexandr <vdovicenco.alexandr@gmail.com>
2018-10-02 14:51:06 +0300

    Refactored Abstract factory to use concrete factory of Plant

---
+++ LAB1(Creational)/src/AbstractPlantFactory.kt
@@ -0,0 +1,11 @@
+
+abstract class AbstractPlantFactory {
+
+    companion object {
+        inline fun <reified T : Plant> createFactory(): PlantFactory = when (T::class) {
+            OrangePlant::class -> OrangeFactory()
+            ApplePlant::class  -> AppleFactory()
+            else               -> throw IllegalArgumentException()
+        }
+    }
+}
--- LAB1(Creational)/src/PlantFactory.kt
+++ LAB1(Creational)/src/PlantFactory.kt
@@ -6,14 +6,6 @@ class ApplePlant : Plant
 
 abstract class PlantFactory {
     abstract fun makePlant(): Plant
-
-    companion object {
-        inline fun <reified T : Plant> createFactory(): PlantFactory = when (T::class) {
-            OrangePlant::class -> OrangeFactory()
-            ApplePlant::class  -> AppleFactory()
-            else               -> throw IllegalArgumentException()
-        }
-    }
 }
 
 class AppleFactory : PlantFactory() {
