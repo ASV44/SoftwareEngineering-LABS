4bb33a9c76e8a42e4861b39e60bce3fbfd58d19d
Vdovicenco Alexandr <vdovicenco.alexandr@gmail.com>
2018-10-02 14:06:08 +0300

    Added "Builder" pattern usage

--- LAB1(Creational)/src/Builder.kt
+++ LAB1(Creational)/src/Builder.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 class Dialog() {
 
     fun showTitle() = println("showing title")
--- LAB1(Creational)/src/main.kt
+++ LAB1(Creational)/src/main.kt
@@ -1,5 +1,26 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     val plantFactory = PlantFactory.createFactory<OrangePlant>()
     val plant = plantFactory.makePlant()
     println("Created plant: $plant")
+
+    val dialog: Dialog = dialog {
+        title {
+            text = "Dialog Title"
+        }
+        message {
+            text = "Dialog Message"
+            color = "#333333"
+        }
+        image {
+            File.createTempFile("image", "jpg")
+        }
+    }
+
+    dialog.show()
+}
+
+fun dialog(init: DialogBuilder.() -> Unit): Dialog {
+    return DialogBuilder(init).build()
 }
