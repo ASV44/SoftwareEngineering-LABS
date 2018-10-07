01162a6322709ec713abe18f83b366af52ebd9d8
Vdovicenco Alexandr <vdovicenco.alexandr@gmail.com>
2018-10-07 21:17:22 +0300

    Refactored "Builder" to "PlantBuilder" as inner class of base "Plant" class, moved "OrangePlant" and "ApplePlant" to Plant.kt file

---
+++ LAB1(Creational)/src/Plant.kt
@@ -0,0 +1,63 @@
+open class Plant(var name: String = "",
+            var species: String = "",
+            var genus: String = "",
+            var family: String = "",
+            var order: String = "") {
+
+    fun showName() = println("Plant name: $name")
+
+    fun showSpecies() = println("Plant species: $species")
+
+    fun showGenus() = println("Plant genus: $genus")
+
+    fun showFamily() = println("Plant family: $family")
+
+    fun showPlantDetails() {
+        showName()
+        showSpecies()
+        showGenus()
+        showFamily()
+    }
+
+    //Builder:
+    open class PlantBuilder(private var plant: Plant) {
+
+        fun setName(name: String): PlantBuilder {
+            plant.name = name
+            return this
+        }
+
+        fun setSpecies(species: String): PlantBuilder {
+            plant.species = species
+            return this
+        }
+
+        fun setGenus(genus: String): PlantBuilder {
+            plant.genus = genus
+            return this
+        }
+
+        fun setFamily(family: String): PlantBuilder {
+            plant.family = family
+            return this
+        }
+
+        fun setOrder(order: String): PlantBuilder {
+            plant.order = order
+            return this
+        }
+
+        fun build(): Plant {
+            return plant
+        }
+
+    }
+}
+
+class OrangePlant : Plant() {
+    class OrangePlantBuilder: PlantBuilder(OrangePlant())
+}
+
+class ApplePlant : Plant() {
+    class ApplePlantBuilder: PlantBuilder(ApplePlant())
+}
